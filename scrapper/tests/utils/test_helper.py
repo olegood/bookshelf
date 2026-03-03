31669e244733c746f2223c5ba2214b0ed601e332
Oleg Anastassov <oleg.anastassov@gmail.com>
2026-03-04 00:10:24 +0100

    test: fix parametrized arguments and change formatting

--- scrapper/tests/utils/test_helper.py
+++ scrapper/tests/utils/test_helper.py
@@ -3,12 +3,26 @@ import pytest
 from scrapper.src.utils.helper import is_url_line
 
 
-@pytest.mark.parametrize("url", ["https://www.amazon.com/dp/1780818720?ref=ppx_yo2ov_dt_b_fed_asin_title",
-                                 "   https://www.amazon.com/dp/1780818720   "])
-def test_is_url_valid_url(url: str):
-    assert is_url_line(url)
+@pytest.mark.parametrize(
+    "url_line",
+    [
+        "https://www.amazon.com/dp/1780818720?ref=ppx_yo2ov_dt_b_fed_asin_title",
+        "   https://www.amazon.com/dp/1780818720   "
+    ]
+)
+def test_is_url_line_valid(url_line: str):
+    assert is_url_line(url_line)
 
 
-@pytest.mark.parametrize("url", ["", "   ", "# This is a comment ", "#", "   # Leading whitespace with comment"])
-def test_is_url_invalid_url(url: str):
-    assert not is_url_line(url)
+@pytest.mark.parametrize(
+    "url_line",
+    [
+        "",
+        "   ",
+        "# This is a comment ",
+        "#",
+        "   # Leading whitespace with comment"
+    ]
+)
+def test_is_url_line_invalid(url_line: str):
+    assert not is_url_line(url_line)
