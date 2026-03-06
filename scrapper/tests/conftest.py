59a21ff13986116f30e9567e79c55224a9c5f4b4
Oleg Anastassov <oleg.anastassov@gmail.com>
2026-03-06 23:00:46 +0100

    docs: add docstring to log_time fixture

--- scrapper/tests/conftest.py
+++ scrapper/tests/conftest.py
@@ -9,6 +9,16 @@ def pytest_addoption(parser):
 
 @pytest.fixture(autouse=True)
 def log_time(request):
+    """
+    log_time is a pytest fixture that automatically gets applied to every test function. It is used
+    to measure and log the execution time of test functions if the logging is enabled via a
+    command-line option. The fixture records the time at the start of the test function execution
+    and displays the elapsed time upon completion of the test.
+
+    :param request: A pytest request object that provides access to the test node and configuration
+        details.
+    :return: None
+    """
     enabled = request.config.getoption("--logtime")
 
     if enabled:
